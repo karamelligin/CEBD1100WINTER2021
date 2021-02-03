34a3303490235f7761d7fbbff0b23d4ba8ed7597
karamelligin <gingermelli@icloud.com>
2021-02-03 15:08:50 -0500

    practice

---
+++ CLASS 2/practice3feb2021.py
@@ -0,0 +1,22 @@
+
+print("Hello February")
+message = "Hello February"
+print(message)
+message = 'howdy'
+print (message.title())
+print (message.upper())
+print (message)
+print (message.lower())
+
+first_name = "tina"
+last_name = "smith"
+full_name = "{} {}".format(first_name, last_name)
+print (full_name.title())
+
+celsius = -8
+f = 1.8 * celsius + 32
+print ("The temperature in f is", f)
+
+
+
+
